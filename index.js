be3aaa7b9673dabd102dadd16b3591f68d6e6687
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-23 10:23:53 +0200

    debugging

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ class ProjectManager {
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
+// if (typeof exports !== 'undefined') {
   module.exports = module.exports || {}
   if (Airplane) { module.exports.Airplane = Airplane }
   if (Person) { module.exports.Person = Person }
@@ -153,4 +153,4 @@ if (typeof exports !== 'undefined') {
   if (Instructor) { module.exports.Instructor = Instructor }
   if (Student) { module.exports.Student = Student }
   if (ProjectManager) { module.exports.ProjectManager = ProjectManager }
-}
+// }
