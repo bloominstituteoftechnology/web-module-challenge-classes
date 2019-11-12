56db4e45698326858fa92dfaec07f547ca3977e2
Cassandra Dew <cpdew2@gmail.com>
2019-11-11 21:14:20 -0500

    project submission1

--- index.js
+++ index.js
@@ -41,8 +41,28 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
+  eat(someFood) {
+    if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}  
+    
+  poop(){
+   this.stomach = []
+  }
+
+  toString() {
+  return `${this.name}, ${this.age}`
+
+  }
 }
+
 
 /*
   TASK 2
@@ -59,8 +79,25 @@ class Person {
 */
 
 class Car {
+  constructor (model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
-}
+  fill(gallons){
+    this.tank = this.tank += gallons;
+  }
+
+  drive(distance){
+    this.odometer += distance;
+    this.tank -= (distance / this.milesPerGallon);
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+ }
+
+
 
 /*
   TASK 3
@@ -75,7 +112,16 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor (Obj) {
+    Obj = Obj;
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
 
+  speak (name, location){
+    return `Hello my name is ${name}, I am from ${location}.`;
+  }
 }
 
 /*
@@ -92,8 +138,13 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(object){
+    super (object);
+    this.name = object.name;
 
+
+  }
 }
 
 /*
@@ -111,8 +162,8 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  
 }
 
 /*
@@ -128,7 +179,7 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor{
 
 }
 
