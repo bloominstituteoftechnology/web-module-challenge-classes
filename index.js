113e2410dc2a3d007392fa11364f2d3e8918b509
c0der.br1t <c0der.br1t@gmail.com>
2019-11-14 14:43:27 -0600

    10 tests passing, including both example tests. Finished all 8 'Instances of Person'.

--- index.js
+++ index.js
@@ -41,8 +41,29 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
+  eat(edible){
+    if (this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
+
+/*
+
+*/
 
 /*
   TASK 2
