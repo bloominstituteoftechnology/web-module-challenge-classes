6ff3c0a4e6b162d6f046a7e4de7cfe8fad42d18b
c0der.br1t <c0der.br1t@gmail.com>
2019-11-14 15:03:09 -0600

    46 tests passing, including both example tests. Finished all 11 'Instances of Student'.

--- index.js
+++ index.js
@@ -173,8 +173,26 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(args){
+    super(args);
+    this.previousBackground = args.previousBackground;
+    this.className = args.className;
+    this.favSubjects = args.favSubjects;
+  }
 
+  listSubjects(){
+    let stringed = this.favSubjects.join(', ');
+    return `Loving ${stringed}!`;
+  }
+
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject){
+    return `${this.name} has begun spring challenge on ${subject}`;
+  }
 }
 
 /*
