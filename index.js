38a5c1934c91dc73b1037335587ec3deb57f9380
christina kaliora <develop@pirron.one>
2019-11-14 20:05:37 +0100

    26 passing

--- index.js
+++ index.js
@@ -41,7 +41,21 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name,
+    this.age = age,
+    this.stomach = [];
+  }
+  eat(food){
+    if (this.stomach.length >= 10) return;
+    this.stomach.push(food);
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name} ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +73,28 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0
+  }
+  fill(gallons){
+    return this.tank += gallons;
+  }
+  drive(distance){
+    let burnedGallons = distance / this.milesPerGallon;
 
+    if( burnedGallons > this.tank ){
+      burnedGallons = this.tank;
+      distance = this.tank * this.milesPerGallon;
+    }
+
+    this.odometer += distance;
+    this.tank -= burnedGallons;
+
+    if (this.tank === 0) return `I ran out of fuel at ${this.odometer} miles!`;
+  }
 }
 
 /*
@@ -75,7 +110,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
