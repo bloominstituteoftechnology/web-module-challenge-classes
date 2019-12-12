4b7677eb558a2d72ceff63c0213d5ab2148e0feb
Pedro Revilla <peterevilla@gmail.com>
2019-12-11 22:39:05 -0600

    work in progress

--- index.js
+++ index.js
@@ -41,7 +41,28 @@ class Airplane {
 */
 
 class Person {
+    constructor(name, age) {
 
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+
+    eat(someFood) {
+
+      if(this.stomach.length < 10) {
+      this.stomach.push(someFood)
+    } 
+  }
+    poop() {
+
+      this.stomach = [];
+    }
+
+    toString() {
+
+      return `${this.name}, ${this.age}`
+    }
 }
 
 /*
@@ -59,7 +80,22 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.milesPerGallon = milesPerGallon;
+    this.model = model;
+    this.tank = 0;
+    this.odometer = 0;
+  }
 
+  fill(gallons) {
+
+    this.tank += gallons;
+  }
+
+  drive() {
+
+    this.odometer += distance;
+  }
 }
 
 /*
