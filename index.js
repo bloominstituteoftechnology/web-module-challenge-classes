156c6ae3e9734e8b76055bd6d4713c8b893e4917
marie-hotchkiss <mtknowlton67@gmail.com>
2019-12-12 14:42:03 -0800

    passed Classes Lambdian/Instructor extends

--- index.js
+++ index.js
@@ -94,7 +94,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+   speak() {
+     return `Hello, my name is ${this.name}, I am from ${this.location}`;   
+    }
 }
 
 /*
@@ -111,9 +118,25 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
-}
+class Instructor extends Lambdasian {
+  constructor(attributes){
+ super(attributes)
+ this.specialty= attributes.specialty;
+ this.favLanguage = attributes.favLanguage;
+ this.catchPhrase = attributes.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`
+  }
+  grade(student,  subject){
+ return `${student.name} receives a perfect score on ${subject}`
+  }
+ }
+ 
+ 
+ 
+ 
+ 
 
 /*
   TASK 5
