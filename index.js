82acc7b7d276a8a2bf9a54ea2f55d3146b87a210
Amanda Hogan <ahogan@teamsynced.com>
2019-12-18 11:49:03 -0500

    Project Manager solution

--- index.js
+++ index.js
@@ -207,9 +207,22 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
 
-}
+class ProjectManager extends Instructor{
+  constructor(pmAttrs) {
+    super(pmAttrs);
+    this.gradClassName = pmAttrs.gradClassName;
+    this.favInstructor = pmAttrs.favInstructor;
+  };
+
+  standUp(channel) {
+    return `${this.name} announces to ${channel} @channel standy times!`;
+  };
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  };
+};
 
 /*
   STRETCH PROBLEM (no tests!)
