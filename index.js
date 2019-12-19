aaa74a003eed84af38c8da2559480c411d9b74f7
Cedric Winbush <cawinbushjr@gmail.com>
2019-12-19 16:53:59 -0600

    Finish Student, Lambdasian, and Instructor

--- index.js
+++ index.js
@@ -40,9 +40,51 @@ class Airplane {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-class Person {
+// function Person(name, age) {
+//   this.stomach = [];
+//   this.age = age;
+//   this.name = name;
+    
+//   }
+//   Person.prototype.eat = function (someFood){
+//       if(this.stomach.length <=9){
+//         this.stomach.push(someFood);
+//       }
+        
+//     }
 
-}
+//   Person.prototype.poop = function (){
+//     this.stomach.length = 0;
+//   };
+
+//   Person.prototype.toString = function(){
+//     return `${this.name}, ${this.age}`;
+//   }
+
+  class Person {
+    constructor(name, age){
+      this.stomach = [],
+      this.age = age,
+      this.name = name 
+    }
+    
+    eat(someFood){
+      if(this.stomach.length <=9){
+        this.stomach.push(someFood);
+      }
+          
+    }
+  
+    poop(){
+      this.stomach.length = 0;
+    }
+  
+    toString (){
+      return `${this.name}, ${this.age}`;
+    }
+  }
+
+
 
 /*
   TASK 2
@@ -58,7 +100,61 @@ class Person {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
+// function Car(model, milesPerGallon) {
+//   this.model = model;
+//   this.milesPerGallon = milesPerGallon;
+//   this.tank = 0;
+//   this.odometer = 0;
+  
+
+// }
+
+// Car.prototype.fill = function(gallons){
+//   this.tank = this.tank + gallons;
+  
+
+// } 
+
+// Car.prototype.drive = function(distance){
+//   this.odometer = this.odometer + distance;
+//   this.tank= this.tank - (distance/this.milesPerGallon);
+
+//   if(this.tank < 0){
+//     this.tank = 0;
+//     this.odometer = 200;
+//     return `I ran out of fuel at ${this.odometer} miles!`;
+
+//   };
+
+  
+
+// } 
+
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0,
+    this.fulltank=0
+  }
+
+  fill(gallons){
+    this.tank = this.tank + gallons;  
+    this.fulltank = gallons * this.milesPerGallon;
+  } 
+
+  drive(distance){
+    this.odometer = this.odometer + distance;
+    this.tank= this.tank - (distance/this.milesPerGallon);
+
+    if(this.tank < 0){
+      this.tank = 0;
+      this.odometer = this.fulltank;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    };  
+
+  } 
 
 }
 
@@ -75,7 +171,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes){
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -92,7 +195,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(attributes){
+    super(attributes);
+    this.specialty= attributes.specialty,
+    this.favLanguage= attributes.favLanguage,
+    this.catchPhrase = attributes.catchPhrase
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  };
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  };
 
 }
 
@@ -111,7 +226,13 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(attributes){
+    this.previousBackground = attributes.previousBackground,
+    this.className = attributes.previousBackground,
+    this.favSubjects = attributes.favSubjects
+
+  }
 
 }
 
