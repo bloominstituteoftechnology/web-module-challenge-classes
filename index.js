cc286e623a20dc18179f91dc501bdfa7b7254b92
Cedric Winbush <cawinbushjr@gmail.com>
2019-12-19 17:23:14 -0600

    finish remaining Task

--- index.js
+++ index.js
@@ -228,11 +228,20 @@ class Instructor extends Lambdasian{
 */
 class Student extends Lambdasian {
   constructor(attributes){
+    super(attributes);
     this.previousBackground = attributes.previousBackground,
-    this.className = attributes.previousBackground,
+    this.className = attributes.className,
     this.favSubjects = attributes.favSubjects
-
   }
+  listSubjects(){
+    return `Loving ${this.favSubjects}!`;
+  };
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  };
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  };
 
 }
 
@@ -249,7 +258,18 @@ class Student extends Lambdasian {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+  constructor(attributes){
+    super(attributes);
+    this.gradClassName = attributes.gradClassName,
+    this.favInstructor = attributes.favInstructor
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+  };
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 
 }
 
