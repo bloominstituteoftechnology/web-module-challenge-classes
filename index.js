67ea8296a5f98f67695119a352071f4fc98b2350
Gideon <reachgiddy@gmail.com>
2019-12-19 21:12:11 -0500

    test passed for Task4 - Instructor class

--- index.js
+++ index.js
@@ -141,9 +141,26 @@ const gideon = new Lambdasian({
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(attributes){
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
 }
+
+const brit = new Instructor({
+  specialty: 'Web Fundamentals',
+  favLanguage: 'Javascript',
+  catchPhrase: 'use const until you can\'t and then use let'
+});
 
 /*
   TASK 5
