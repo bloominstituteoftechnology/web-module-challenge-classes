bf4e625dd6ae9fd01ce773f08895f2f15622bdd9
Lauren <leladrummonds@gmail.com>
2020-01-24 20:31:40 -0500

    Update index.js
    JS IV - updated index.js

--- index.js
+++ index.js
@@ -41,9 +41,23 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() { 
+    this.stomach.length = 0;
+  }
+  toString() { 
+    return this.name + ", " + this.age;
+  }
 }
-
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -59,8 +73,32 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    let fuelUsed = distance/this.milesPerGallon;
+    if(this.tank <= fuelUsed) {
+      fuelUsed = this.tank;
+      this.tank -= fuelUsed;
+      this.tank = 0;
+      this.odometer += distance - 1;
+      return "I ran out of fuel at " + this.odometer + " miles!";
+    } else {
+      this.odometer += distance;
+      this.tank -= fuelUsed;
+    }
+  }
 }
+
+
+
 
 /*
   TASK 3
@@ -75,7 +113,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor (attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return "Hello my name is " + this.name + ", I am from " + this.location;
+  }
 }
 
 /*
@@ -92,8 +137,22 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor (instructAtts) {
+    super(instructAtts);
+    this.isInstructor = instructAtts.isInstructor;
 
+    this.specialty = instructAtts.specialty;
+    this.favLanguage = instructAtts.favLanguage;
+    this.catchPhrase = instructAtts.catchPhrase;
+
+  }
+  demo(subject) {
+    return "Today we are learning about " + subject;
+  }
+  grade(student,subject) {
+    return student.name + " receives a perfect score on " + subject;
+  }
 }
 
 /*
@@ -111,8 +170,25 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor (studentAtts) {
+    super(studentAtts);
+    this.isStudent = studentAtts.isStudent;
 
+    this.previousBackground = studentAtts.previousBackground;
+    this.className = studentAtts.className;
+    this.favSubjects =studentAtts.favSubjects;
+
+  }
+  listSubjects() {
+    return this.favSubjects.toString();
+  }
+  PRAssignment(subject) {
+    return this.name + " has submitted a PR for " + subject;
+  }
+  sprintChallenge(subject) {
+    return this.name + " has begun sprint challenge on " + subject;
+  }
 }
 
 /*
@@ -128,8 +204,21 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor {
+  constructor (pmAttributes) {
+    super(pmAttributes);
+    this.isProjectManager = pmAttributes.isProjectManager;
 
+    this.gradClassName = pmAttributes.gradClassName;
+    this.favInstructor = pmAttributes.favInstructor;
+
+  }
+  standUp(slack_channel) {
+    return this.name + " announces to " + slack_channel + "@channel standy times!";
+  }
+  debugsCode(student, subject) {
+    return this.name + " debugs " + student.name + "'s code on " + subject;
+  }
 }
 
 /*
