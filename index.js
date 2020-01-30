ffc2f27fd18ef6e6365ce7cfb989a0c3158569ce
Teague Hannam <teaguehannam@gmail.com>
2020-01-30 13:39:56 -0700

    Finished second task

--- index.js
+++ index.js
@@ -66,14 +66,41 @@ class Person {
         + should initialize with a `tank` at 0
         + should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+
     - Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+
     - A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  fill(gallons) {
+    this.tank += gallons;
+  }
+
+  drive(distance) {
+    let maxDistance = this.tank * this.milesPerGallon;
+    let fuelBurned = distance / this.milesPerGallon;
+
+    if(maxDistance >= distance){
+      this.odometer += distance;
+      this.tank -= fuelBurned;
+    }else{
+      this.odometer += maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}`;
+    }
+  }
+
 
 }
 
