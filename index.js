7dda05e4e2d8c9b4439455a784cbd484adfdf61a
Nikki Lin <nikkiylin@gmail.com>
2020-01-30 14:02:08 -0800

    done Student class

--- index.js
+++ index.js
@@ -167,8 +167,22 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(attributes) {
+    super(attributes)
+    this.previousBackground = attributes.previousBackground,
+    this.className = attributes.className,
+    this.favSubjects = attributes.favSubjects
+  }
+  listSubjects() {
+    return `Loving ${this.favSubjects}`;
+  }
+  PRAssignment(this.subject) {
+    return `${this.name} has sunmitted a PR for ${this.subject}`;
+  }
+  sprintChallenge(this.subject) {
+    return `${this.name} has begun sprint challenge on ${this.subject}`;
+  }
 }
 
 /*
