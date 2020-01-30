05ae34d5ab1515a14ecb61fb321b62c0890b0244
Nikki Lin <nikkiylin@gmail.com>
2020-01-30 12:59:13 -0800

    done Lambdasian

--- index.js
+++ index.js
@@ -77,6 +77,25 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+  fill(gallons) {
+    this. tank += gallons;
+  }
+  drive(distance) {
+    if (this.tank * this.milesPerGallon < distance) {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}!`;
+    } else {
+      this.odometer += distance;
+      this.tank -= distance /this.milesPerGallon;
+    }
+  }
 
 }
 
@@ -93,7 +112,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes) {
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
