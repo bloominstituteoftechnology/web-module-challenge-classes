3c29ead935f2c891208de5d97cbc0cfc05a2d8fa
Nikki Lin <nikkiylin@gmail.com>
2020-01-30 12:36:08 -0800

    done the Person class

--- index.js
+++ index.js
@@ -41,6 +41,24 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if (this.stomach.length >= 10) {
+      this.stomach.length = 10;
+    } else {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 
 }
 
