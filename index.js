944b8c1fb6dd4442cd5a121f48f9d3f3a9ec98c5
Jon-Stickrod <jonstickrodCodes@gmail.com>
2020-02-20 10:08:19 -0800

    Lambdasian class

--- index.js
+++ index.js
@@ -75,7 +75,16 @@ class Person {
 */
 
 class Car {
-
+constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon =  milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gal) {
+    return (this.tank += gal);
+  }
+  drive() {}
 }
 
 /*
@@ -91,7 +100,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
