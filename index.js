f534cc5d0791c10a853862362181e0052b4842bb
Mandy Moscoso <ngocquyen11@yahoo.com>
2020-02-20 19:55:20 -0800

    completed task 2

--- index.js
+++ index.js
@@ -76,7 +76,28 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
 
+  drive(distance) {
+    if (this.tank * this.milesPerGallon >= distance){
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
+    }
+    else {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles`;
+  
+    }
+  }
 }
 
 /*
