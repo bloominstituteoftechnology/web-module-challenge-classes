5186947334e3ad5ab127149e85179b4ae7037e92
Gabriel Romero <gabester78@gmail.com>
2020-02-27 09:45:42 -0600

    Halfway done.

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ class Person {
     this.age = age;
     this.stomach =[];
   }
-  eat(someFood){
+  eat(someFood) {
     if(this.stomach.length < 10)
     return this.stomach.push(someFood);
   }
-  poop(){
+  poop() {
     this.stomach.length = 0;
   }
-  toString(){
+  toString() {
     return `${this.name}, ${this.age}`;
   }
 }
@@ -75,7 +75,19 @@ class Person {
 */
 
 class Car {
-
+  constructor(model,milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    return this.tank += gallons;
+  }
+  drive(distance){
+    this.odometer += distance;
+    // if ()
+  }
 }
 
 /*
@@ -91,7 +103,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(name,age,location) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak(){
+    return `Hello my name is ${name}, I am from ${location}`;
+  }
 }
 
 /*
