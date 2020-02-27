9de5fd9da3402c71090b5d46f29b4a9297e0ce61
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-02-27 12:33:33 -0800

    first test passing

--- index.js
+++ index.js
@@ -41,8 +41,24 @@ class Airplane {
 */
 
 class Person {
-
+ constructor(name, age){
+   this.name = name,
+   this.age = age,
+   this.stomach = []
+ }
+eat(someFood){
+  if (this.stomach.length <10) {
+    this.stomach.push(someFood);
+  }
 }
+poop(){
+  this.stomach = []
+}
+toString(){
+ return `${this.name}, ${this.age}`
+}
+}
+
 
 /*
   TASK 2
@@ -59,7 +75,12 @@ class Person {
 */
 
 class Car {
-
+ constructor(model, milesPerGallon){
+   this.tank = 0;
+   this.odometer = 0;
+   this.model = model;
+   this.milesPerGallon;
+ }
 }
 
 /*
