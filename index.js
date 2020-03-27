00ebc58c1241886ff37ddced8d37c7000f19e067
jidelson <58210030+jidelson@users.noreply.github.com>
2020-03-26 19:56:40 -0700

    15 functions passing

--- index.js
+++ index.js
@@ -41,6 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+eat(someFood){
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood); 
+  }
+}
+
+poop(){
+  this.stomach = []
+}
+
+toString(){
+  return `${this.name} and ${this.age}`
+}
 
 }
 
@@ -59,6 +78,16 @@ class Person {
 */
 
 class Car {
+constructor(model, milesPerGallon){
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
+}
+
+fill(gallons){
+this.tank = this.tank += gallons
+}
 
 }
 
