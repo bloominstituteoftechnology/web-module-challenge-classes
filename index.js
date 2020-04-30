df16c4c3edd91f71a64fccd9381014b4b849c4f4
JenVest2020 <jenvest2019@gmail.com>
2020-04-30 16:50:11 -0500

    passed all Lambdasian constructor tasks

--- index.js
+++ index.js
@@ -110,7 +110,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(object) {
+    this.name = object.name;
+    this.age = object.age;
+    this.location = object.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
