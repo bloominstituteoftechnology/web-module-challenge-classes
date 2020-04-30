e778371d6e7d26037071d3c4eeb6481dd7db8e76
JenVest2020 <jenvest2019@gmail.com>
2020-04-30 16:32:52 -0500

    passed all Car constructor tasks

--- index.js
+++ index.js
@@ -74,8 +74,28 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    if (distance > this.tank * this.milesPerGallon) {
+      this.odometer += this.tank * this.milesPerGallon;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+    else {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    }
+  }
 }
+
 
 /*
   TASK 3
