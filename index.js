9cdba8853234f7e55bdb7d36f9795392d15e5ccc
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-05-14 20:02:10 -0400

    task 1 done

--- index.js
+++ index.js
@@ -41,8 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+    poop(){
+      this.stomach = [];
+    }
+    toString(){
+      return `${this.name}, ${this.age}`;
+    }
+  }
 
-}
+
 
 /*
   TASK 2
