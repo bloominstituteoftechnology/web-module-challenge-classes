729b642618853664c606359a7118e1a39e991a7b
Tony Sorensen <tony48853@gmail.com>
2020-06-12 14:27:18 -0700

    All Tasks completed and passing tests

--- index.js
+++ index.js
@@ -200,8 +200,24 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+  constructor(projectManagerAttributes){
+    super(projectManagerAttributes)
+    this.name = projectManagerAttributes.name;
+   this.age = projectManagerAttributes.age;
+   this.location = projectManagerAttributes.location;
+   this.specialty = projectManagerAttributes.specialty;
+   this.favLanguage = projectManagerAttributes.favLanguage;
+   this.catchPhrase = projectManagerAttributes.catchPhrase;
+   this.gradClassName = projectManagerAttributes.gradClassName;
+   this.favInstructor = projectManagerAttributes.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @${channel} standy times!`;
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
 }
 
 /*
