bdfd9e631b0e19eeb4949f30623e9f3e0775173c
Tony Sorensen <tony48853@gmail.com>
2020-06-11 18:08:42 -0700

    Task 1 passing all tests

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+constructor(name,age){
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+eat(food){
+  if(this.stomach.length<10){
+    this.stomach.push(food);
+  }
+};
+poop(){
+  this.stomach=[];
+};
+toString(name,age){
+return `${this.name},${this.age}`;
+}
 }
 
 /*
