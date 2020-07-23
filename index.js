ba8330999ad11e92edb28981837be73318149d0a
Josh Glantz-Hucks <jahteo@gmail.com>
2020-07-23 14:32:51 -0700

    FInished MVP & Stretch. Not entirely sure how to stop the student's graduate() from looping 5,000 times if it's unlucky. I feel like I should add a for loop with a max of 4,999 or something. NOt that the Maths-That-Be would actually let it take that long to exceed 70, but there's definitely an edge case that would break someone's computer eventually.

--- index.js
+++ index.js
@@ -9,14 +9,14 @@
 
 // EXAMPLE SOLUTION CODE:
 class Airplane {
-  constructor(name) {
+  constructor (name) {
     this.name = name;
     this.isFlying = false;
   }
-  takeOff() {
+  takeOff () {
     this.isFlying = true;
   }
-  land() {
+  land () {
     this.isFlying = false;
   }
 }
@@ -41,20 +41,20 @@ class Airplane {
 */
 
 class Person {
-  constructor(name, age){
+  constructor (name, age) {
     this.name = name;
     this.age = age;
     this.stomach = [];
   }
-  eat(food){
+  eat (food) {
     if (this.stomach.length < 10) {
       this.stomach.push(food);
     }
   }
-  poop(){
+  poop () {
     this.stomach = [];
   }
-  toString(){
+  toString () {
     return `${this.name}, ${this.age}`
   }
 }
@@ -75,16 +75,16 @@ class Person {
 */
 
 class Car {
-  constructor(model, milesPerGallon) {
+  constructor (model, milesPerGallon) {
     this.model = model;
     this.milesPerGallon = milesPerGallon;
     this.tank = 0;
     this.odometer = 0;
   }
-  fill(gallons) {
+  fill (gallons) {
     this.tank = this.tank + gallons;
   }
-  drive(distance) {
+  drive (distance) {
     if (distance < this.tank * this.milesPerGallon) {
       this.odometer = this.odometer + distance;
       this.tank = this.tank - distance / this.milesPerGallon;
@@ -115,7 +115,7 @@ class Lambdasian {
     this.age = attrs.age;
     this.location = attrs.location;
   }
-  speak() {
+  speak () {
     return `Hello my name is ${this.name}, I am from ${this.location}`
   }
 }
@@ -134,8 +134,22 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor (attrs) {
+    super (attrs);
+    this.specialty = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+  demo (subject) {
+    return `Today we are learning about ${subject}`
+  }
+  grade (student, subject){
+    return `${student.name} receives a perfect score on ${subject}`
+  }
+  willUnexpectedlyAdjustGrade (student) {
+    student.grade = student.grade + ( Math.round((Math.random() - 0.5) * 200) );
+  }
 }
 
 /*
@@ -153,8 +167,32 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+    constructor (attrs) {
+      super (attrs);
+      this.previousBackground = attrs.previousBackground;
+      this.className = attrs.className;
+      this.favSubjects = attrs.favSubjects;
+      this.grade = 86;
+    }
+    listSubjects () {
+      return `Loving ${this.favSubjects[0]}, ${this.favSubjects[1]}, ${this.favSubjects[2]}!`
+    }
+    PRAssignment (subject) {
+      return `${this.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge (subject) {
+      return `${this.name} has begun sprint challenge on ${subject}`
+    }
+    graduate (instructorOrPM) {
+      instructorOrPM.willUnexpectedlyAdjustGrade(this.student);
+      if (this.grade < 70) {
+        console.log(`Better luck next time!`);
+        instructorOrPM.willUnexpectedlyAdjustGrade(this.student);
+      } else {
+        return `Congrats! You studied hard & the Mighty-Random-Maths-That-Be approve of your graduation! Here's a cookie. Next in line, please!`
+      }
+    }
 }
 
 /*
@@ -170,8 +208,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor (attrs) {
+    super (attrs);
+    this.gradClassName = attrs.gradClassName;
+    this.favInstructor = attrs.favInstructor;
+  }
+  standUp (channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+  debugsCode (student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
 
 /*
