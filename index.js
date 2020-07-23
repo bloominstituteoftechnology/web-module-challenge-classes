8fa1d22a0032584d7f9ceae8b7d815c0d6b63794
Josh Glantz-Hucks <jahteo@gmail.com>
2020-07-23 12:42:51 -0700

    Broken answer for Task 1 or tester not working right.

--- index.js
+++ index.js
@@ -41,7 +41,16 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(food){
+    if (this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
 }
 
 /*
