0b49f77152c26fcb68eaf49ef4c6556128fe0f10
aedevdesigns@gmail.com <aedevdesigns@gmail.com>
2020-08-13 21:19:40 -0500

    First task done

--- index.js
+++ index.js
@@ -41,7 +41,25 @@ class Airplane {
 */
 
 class Person {
+ constructor(name, age){
+   this.name = name;
+   this.age = age;
+   this.stomach = [];
+ }
 
+ eat(someFood){
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood)
+  }
+ }
+
+ poop(){
+   return this.stomach.length = 0;
+ }
+
+ toString(){
+  return `${this.name}, ${this.age}`
+}
 }
 
 /*
