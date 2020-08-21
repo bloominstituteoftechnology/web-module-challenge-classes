709fe53375f218f324d792a68c815fc8b054aced
Simone Ballard <simone.a.ballard@gmail.com>
2020-08-21 01:03:52 -0600

    Task 5 updated

--- index.js
+++ index.js
@@ -133,7 +133,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(attributes){
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about {subject}`
+  }
+  grade(student, subject) {
+    return `{student.name} receives a perfect score on {subject}`
+  }
 
 }
 
