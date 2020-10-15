a55073e4a94c0dc3ffee3a603b4f8efcf75faff5
Joshua Lovins <joshualovins@joshuas-mbp.home>
2020-10-15 19:57:53 -0400

    Not quite done with MVP. Got a late start. Should be able to update the push with finished MVP within an hour or so.

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(nm, ag){
+    this.name = nm;
+    this.age = ag;
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10){
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +74,27 @@ class Person {
 */
 
 class Car {
-
+  constructor(md, mpg) {
+    this.model = md;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    const gallonsForDistance = (distance / this.milesPerGallon);
+    const distanceDrivable = this.tank * this.milesPerGallon;
+    if (distanceDrivable < distance){
+      this.odometer += distanceDrivable;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}`;
+    }else{
+      this.odometer += distance;
+      this.tank = this.tank - gallonsForDistance;
+    }
+  }
 }
 
 /*
@@ -75,7 +110,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
@@ -92,8 +134,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(instructorObj){
+    super(instructorObj);
+    this.specialty = instructorObj.specialty;
+    this.favLanguage = instructorObj.favLanguage;
+    this.catchPhrase = instructorObj.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+  grade(studentObj, subject){
+    return `${studentObj.name} receives a perfect score on ${subject}`;
+  }
 }
 
 /*
@@ -111,8 +164,16 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(obj){
+    super(obj);
+    this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  }
+  listSubjects{
+    return `Loving ${this.favSubjects.forEach(element => return element);}`
+  }
 }
 
 /*
