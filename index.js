805b1806fbc4ef2862ef411ee96ede39188226b7
Diego Roman <diegormnv209@gmail.com>
2020-10-15 16:03:36 -0700

    Done MVP

--- index.js
+++ index.js
@@ -174,11 +174,11 @@ class Student extends Lambdasian{
   listSubjects(){
     return `Loving ${this.favSubjects}!`
   }
-  PRAssignment(){
-
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
   }
-  sprintChallenge(){
-
+  sprintChallenge(subject){
+  return `${this.name} has begun sprint challenge on ${subject}`
   }
 }
 
@@ -195,8 +195,18 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(obj){
+    super(obj);
+    this.gradClassName = obj.gradClassName;
+    this.favInstructor = obj.favInstructor;
+  }
+  standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+  }
+  debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+  }
 }
 
 /*
