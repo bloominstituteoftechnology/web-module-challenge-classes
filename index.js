aa9b415a4efa931bed697fc777e9efa6cc33e113
sebastian espeset <sebastian.c.espeset@gmail.com>
2020-10-15 18:20:34 -0700

    solved up to task6

--- index.js
+++ index.js
@@ -118,7 +118,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+      constructor(obj){
+        this.name=obj.name;
+        this.age=obj.age;
+        this.location=obj.location;
+      }
+      speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+      }
 }
 
 /*
@@ -135,8 +142,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+      constructor(insObj){
+        super(insObj);
+        this.specialty=insObj.specialty;
+        this.favLanguage=insObj.favLanguage;
+        this.catchPhrase=insObj.catchPhrase;
+      }
+      demo(str){
+       return `Today we are learning about ${str}`;
+      }
+      grade(obj,subject){
+       return `${obj.name} receives a perfect score on ${subject}`;
+      }
 }
 
 /*
@@ -154,7 +172,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(obj){
+    super(obj);
+    this.previousBackground=obj.previousBackground;
+    this.className=obj.className;
+    this.favSubjects=obj.favSubjects;
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}!`;
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
 
 }
 
