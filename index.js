7e9f5896561a3092d00770e2fc7f42aa11904a99
Molly Brown <mollybrown0516@gmail.com>
2020-11-04 22:09:11 -0500

    Completed first task

--- index.js
+++ index.js
@@ -41,8 +41,32 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible){
+    if (this.stomach.length < 10){
+    this.stomach.push(edible);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
+  
+};
 
-}
+const Neo = new Person ({
+  name: 'Neo',
+  age: 20,
+  stomach: []
+});
+
+console.log(Neo.eat());
 
 /*
   TASK 2
