333e9ec3551d1dbf7c8fa9ced2892462f4a7d8a3
Molly Brown <mollybrown0516@gmail.com>
2020-11-05 13:45:45 -0500

    completed up to task 5

--- index.js
+++ index.js
@@ -93,10 +93,16 @@ class Car {
     this.tank += gallons;
   }
   drive(distance){
-    
-    this.odometer += distance;
-    this.tank -= distance / this.milesPerGallon;
-    return `I ran out of fuel at ${this.odometer} miles!`;
+    const maxDistance = this.tank * this.milesPerGallon;
+    if(distance >= maxDistance) {
+      // I RAN OUT OF GAS
+      this.odometer += maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    } else {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    }
   }
 };
 
@@ -137,9 +143,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+  constructor ({name, age, location, specialty, favLanguage, catchPhrase}){
+    super({name, age, location, specialty, favLanguage, catchPhrase});
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  }
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+};
 
-}
 
 /*
   TASK 5
@@ -156,9 +174,24 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+  constructor({ name, age, location, previousBackground, className, favSubjects }){
+    super({name, age, location, previousBackground, className, favSubjects})
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}!`;
 
-}
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+};
 
 /*
   TASK 6
