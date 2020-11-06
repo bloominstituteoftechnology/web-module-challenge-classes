6af0b5318360773bf5173ee19766e5ff93ed4be4
daniel-vazquez <da.vazquez728@gmail.com>
2020-11-05 18:49:40 -0700

    finished 2/3 of task 2, will come back to last part later

--- index.js
+++ index.js
@@ -85,11 +85,19 @@ console.log(Dan.eat());
 
 class Car {
   constructor(model, milesPerGallon){
-    this.model = model;
-    this.milesPerGallon = milesPerGallon;
-    this.tank = 0;
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+  fill (gallons){
+    this.tank += gallons;
+  }
+
+  drive (distance){
+    
+  }  
 /*
   TASK 3
     - Write a Lambdasian class.
