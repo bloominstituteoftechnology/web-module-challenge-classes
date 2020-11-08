1737e1e5348cd59a476865ea4dbae63e0a9d437b
Ethan Karp <ethansmainemail@gmail.com>
2020-11-07 20:28:47 -0700

    finished task 3

--- index.js
+++ index.js
@@ -105,8 +105,16 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(obj) {
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
+  }
 }
+
 
 /*
   TASK 4
