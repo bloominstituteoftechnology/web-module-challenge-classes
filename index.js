96c7c27c8fad4eee5b4d440b04119d09002b4497
unknown <nick.torres213@gmail.com>
2020-11-09 21:45:39 -0500

    task 2

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +74,24 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    if (this.tank >= 0 ) {
+      this.odometer += distance;
+      this.tank -= (distance / this.milesPerGallon);
+    }
+     {
+    return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
