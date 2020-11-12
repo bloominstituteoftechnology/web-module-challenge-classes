46da5d82169a886842028ab98e984f1c2308f754
William Dye <williamdye@Williams-MacBook-Pro.local>
2020-11-12 02:23:10 -0500

    Finished task 1

--- index.js
+++ index.js
@@ -41,9 +41,27 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+
+  poop() {
+    this.stomach = []
+  }
+
+  toString() {
+    return `${this.name}, ${this.age}`
+  }
 
 }
-
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
