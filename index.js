4bc1d8d130dfdca6cac785313a7774953f694756
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2020-11-12 20:58:35 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -29,38 +29,91 @@ class Airplane {
 
 /*
   TASK 1
-    - Write a Person class whose constructor initializes `name` and `age` from arguments.
-    - All instances of Person should also initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
-        + When eating an edible, it should be pushed into the `stomach`.
-        + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
-        + When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
-        + It should return a string with `name` and `age`. Example: "Mary, 50"
+    -[x] Write a Person class whose constructor initializes `name` and `age` from arguments.
+    -[x] All instances of Person should also initialize with an empty `stomach` array.
+    -[x] Give instances of Person the ability to `.eat("someFood")`:
+      [x]  + When eating an edible, it should be pushed into the `stomach`.
+      [x]  + The `eat` method should have no effect if there are 10 items in the `stomach`.
+    -[x] Give instances of Person the ability to `.poop()`:
+      [x]  + When an instance poops, its `stomach` should empty.
+    -[] Give instances of Person a method `.toString()`:
+       [] + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible){
+    if(this.stomach.length < 10){
+      return this.stomach.push(edible);
+    }
+  }
 
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 }
+const personOne = new Person('Tim', 29);
+console.log(personOne.toString());
+
+personOne.eat('pizza');
+personOne.eat('taco');
+personOne.eat('hamburger');
+
+console.log(personOne.stomach);
+
+personOne.poop();
+
+console.log(personOne.stomach);
+
 
 /*
   TASK 2
-    - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
-        + should initialize with a `tank` at 0
-        + should initialize with an `odometer` at 0
-    - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+    -x Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
+    -x All instances built with Car:
+       x + should initialize with a `tank` at 0
+       x + should initialize with an `odometer` at 0
+    - xGive cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
     - Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
         + Should cause the the `tank` to go down taking `milesPerGallon` into account.
     - A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
+
+
 */
 
-class Car {
-
+class Car{
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    return this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    const range = this.tank * this.milesPerGallon;
+    if(distance > range){
+      this.tank = 0;
+      this.odometer = this.odometer + range;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+    else {
+      this.odometer = this.odometer + distance;
+      this.tank = this.tank - (distance/ this.milesPerGallon);
+    }
+  }
 }
+  const carOne =  new Car('toyota', 10)
+  console.log(carOne.drive);
 
 /*
   TASK 3
@@ -74,14 +127,24 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {
-
+class Lambdasian{
+  constructor(lambda){
+    this.name = lambda.name;
+    this.age = lambda.age;
+    this.location = lambda.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 }
 
+const personTwo = new Lambdasian({ name: 'Tim', age: 29, location: 'Naples'});
+
+// personTwo.speak();
 /*
   TASK 4
-    - Write an Instructor class extending Lambdasian.
-    - Its constructor takes a single argument - an object with the following keys:
+    -x Write an Instructor class extending Lambdasian.
+    - xIts constructor takes a single argument - an object with the following keys:
         + All the keys used to initialize instances of Lambdasian.
         + `specialty`: what the instance of Instructor is good at, i.e. 'redux'
         + `favLanguage`: i.e. 'JavaScript, Python, Elm etc.'
@@ -92,8 +155,22 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor(stats){
+    super(stats);
+    this.specialty = stats.specialty;
+    this.favLanguage = stats.favLanguage;
+    this.catchPhrase = stats.catchPhrase;
+  }
+  demo(subject){
+    this.subject = subject;
+    return `Today we are learning about ${this.subject}`
+  }
+  grade(student, subject){
+    this.student = student.student;
+    this.subject = subject;
+    return `${student.name} receives a perfect score on ${this.subject}`
+  }
 }
 
 /*
@@ -111,7 +188,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian{
+  constructor(past){
+    super(past);
+    this.previousBackground = past.previousBackground;
+    this.className = past.className;
+    this.favSubjects = past.favSubjects;
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}`;
+  }
+  PRAssignment(task){
+    return `${this.name} has submitted a PR for ${task}`
+  }
+  sprintChallenge(sprinting){
+    return `${this.name} has begun sprint challenge on ${sprinting}`;
+  }
 
 }
 
@@ -128,8 +220,20 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor{
+  constructor(past){
+    super(past);
+    this.gradClassName = past.gradClassName;
+    this.favInstructor = past.favInstructor;
+  }
+  standUp(slackChannel){
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+  }
+  debugsCode(student, subject){
+    this.student = student.student;
+    this.subject = subject;
+    return `${this.student} debugs ${student.name}'s code on ${this.subject}`;
+  }
 }
 
 /*
