1c2abdf1d1264b05798ee982cba406ed37d3a335
Stan Tudor <admin@Mac.local>
2020-12-10 19:29:56 -0800

    Task-5 completed

--- index.js
+++ index.js
@@ -163,8 +163,22 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(attr) {
+    super(attr);
+    this.previousBackground = attr.previousBackground;
+    this.className = attr.className;
+    this.favSubjects = attr.favSubjects;
+  }
+  listSubjects() {
+    return `loving ${this.favSubjects}, ${this.favSubjects}, ${this.favSubjects}!`
+  }
+ PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
 /*
