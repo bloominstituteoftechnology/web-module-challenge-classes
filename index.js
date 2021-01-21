be7f748e1a4068fd8797473dae57f7866558dbfc
Tee Chandy <73080544+tpchandy@users.noreply.github.com>
2021-01-21 18:36:48 -0500

    task 1 completed and passing all tests.

--- index.js
+++ index.js
@@ -42,8 +42,27 @@ class Airplane {
   */
   
  class Person {
-    
-  }
+  
+    constructor(name,age) {
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat (someFood) {
+      if (this.stomach.length < 10) {
+        this.stomach.push (someFood);
+      }
+    }
+    poop () {
+      this.stomach.splice (0);
+      return this.stomach
+    }
+    toString () {
+      return `${this.name} , ${this.age}`;
+    }
+     
+   } 
+  
   
   /*
     TASK 2
