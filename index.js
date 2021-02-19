6b1dd1378847a2df735e9c2c4b9f1b3df61ee5c4
anushazia <anusha.zi@gmail.com>
2021-02-18 21:42:57 -0500

    solved the first 2 tasks

--- index.js
+++ index.js
@@ -42,7 +42,23 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+  }
+    eat(someFood) {
+      if (this.stomach.length < 10) {
+        this.stomach.push(someFood);
+      }
+      
+  }
+    poop() {
+      this.stomach = [];
+  }
+    toString() {
+      return `${this.name}, ${this.age}`
+    }
   }
   
   /*
@@ -60,7 +76,24 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+
+    fill(gallons) {
+      this.gallons += this.tank;
+    }
+
+    drive(distance) {
+      this.distance +- this.odometer;
+      this.distance -= this.tank / this.milesPerGallon;
+      if (this.tank === 0) {
+        return `I ran out of fuel at ${this.odometer} miles!`
+      }
+    }
   }
   
   /*
