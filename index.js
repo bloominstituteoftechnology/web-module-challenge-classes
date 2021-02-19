fbbf48f0a57bceee128b395fda9e3db5bdad4363
anushazia <anusha.zi@gmail.com>
2021-02-19 02:36:28 -0500

    some tests not totally correct

--- index.js
+++ index.js
@@ -200,9 +200,21 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
+ class ProjectManager extends Instructor {
+     constructor(attr) {
+       super(attr);
+       this.gradClassName = attr.gradClassName;
+       this.favInstructor = attr.favInstructor;
+     }
      
- }
+     standUp(name) {
+       return `${this.name} announces to ${channel}, @channel standy times!`
+     }
+
+     debugsCode(attr, subject) {
+       return `${this.name} debugs ${student.name}'s code on ${subject}`
+     }
+    }
   /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
