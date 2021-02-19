8ac208f3bace61bdbb4a81f054930e6589c6d7f6
Greg Brewton <gbrewton1980@gmail.com>
2021-02-18 18:55:51 -0700

    Completes Task 4

--- index.js
+++ index.js
@@ -135,8 +135,15 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+    constructor(props) {
+        super(props);
+        this.specialty = props.specialty;
+        this.favLanguage = props.favLanguage;
+        this.catchPhrase = props.catchPhrase;
+    }
+    demo(subject) { return `Today we are learning about ${subject}` }
+    grade(student, subject) { return `${student.name} receives a perfect score on ${subject}` }
 }
 /*
   TASK 5
