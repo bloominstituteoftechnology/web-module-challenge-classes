a036b40e1ab86f681f223952c3b8b3fa836569b9
anushazia <anusha.zi@gmail.com>
2021-02-18 22:47:12 -0500

    got a few more npm tests running

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ class Airplane {
     }
 
     drive(distance) {
-      this.distance +- this.odometer;
+      this.distance += this.odometer;
       this.distance -= this.tank / this.milesPerGallon;
       if (this.tank === 0) {
         return `I ran out of fuel at ${this.odometer} miles!`
@@ -109,6 +109,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
+    constructor(attr) {
+      this.name = attr.name;
+      this.age = attr.age;
+      this.location = attr.location;
+    }
+    speak() {
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
     
   }
   
@@ -126,7 +134,13 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian {
+   constructor (attr) {
+     super (attr);
+     this.speciality = this.speciality;
+     this.favLanguage = this.favLanguage;
+     this.catchPhrase = this.catchPhrase;
+   }
 
  }
   /*
