2c8e9b0a029b7ba949e827e0a5e8f7f2692dff54
Aaron <aarongabriel147@gmail.com>
2021-02-18 20:21:02 -0800

    a bit lost to sya the least.

--- index.js
+++ index.js
@@ -24,28 +24,53 @@ class Airplane {
   /*
     TASK 1
       - Write a Person class whose constructor initializes `name` and `age` from arguments.
+
       - All instances of Person should also initialize with an empty `stomach` array.
       - Give instances of Person the ability to `.eat("someFood")`:
           + When eating an edible, it should be pushed into the `stomach`.
           + The `eat` method should have no effect if there are 10 items in the `stomach`.
+
+
       - Give instances of Person the ability to `.poop()`:
           + When an instance poops, its `stomach` should empty.
       - Give instances of Person a method `.toString()`:
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
-  function Person(name, age) {
+  class Person {
+    constructor (name, age) {
     this.stomach = [];
     this.name = name;
     this.age = age;
+    toString() {
+      return '${this.name}, ${this.age}.'
+    }
+    }
   }
-Person.prototype.eat = (someFood) {
-  if(this.stomach < 10) {
-    this.stomach.push(someFood);
-  } 
+class Eater extends Person {
+  constructor(name, age) {
+    super(name, age);
+    
+  }
 }
-Person.prototype.poop = () {
-  this.
-}
+
+
+
+
+
+
+  //  instance........ const x = new Person() ????????????????????? iono
+
+// Person.prototype.eat = (someFood) {
+//   if(this.stomach < 10) {
+//     this.stomach.push(someFood);
+
+
+
+
+
+
+
+
 
 
 
@@ -54,21 +79,36 @@ Person.prototype.poop = () {
   /*
     TASK 2
       - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
+
       - All instances built with Car:
           + should initialize with a `tank` at 0
           + should initialize with an `odometer` at 0
+
       - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
       - Give cars ability to `.drive(distance)`. The distance driven:
           + Should cause the `odometer` to go up.
           + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+
       - A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
  class Car {
-    
+   constructor (model, milesPerGallon){
+     this.tank = 0;
+     this.odometer = 0;
+     fill (gallons){
+      this.tank += gallons
+     }
+     drive(distance){
+
+     }
+   }
   }
-  
+
+
+
+
   /*
     TASK 3
       - Write a Lambdasian class.
@@ -82,9 +122,29 @@ Person.prototype.poop = () {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
+   constructor(x){
+     this.name = 
+     this.age = 
+     this.location = 
+
+   }
     
   }
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /*
     TASK 4
       - Write an Instructor class extending Lambdasian.
