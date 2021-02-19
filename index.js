ab7465d2d2525673810ea1c5256ebf0661e247a2
vida <ofo28@aol.com>
2021-02-18 21:00:10 -0500

    Task 1 finish

--- index.js
+++ index.js
@@ -42,8 +42,23 @@ class Airplane {
   */
   
  class Person {
-    
-  }
+  constructor(name) {
+     this.name = name.name;
+     this.age = name.age;
+    this.stomach = []
+   }
+     eat(someFood) {
+   if (this.stomach.length < 10) {
+     this.stomach.push(someFood)
+   }
+ }
+ poop() {
+   this.stomach = []
+ }
+ toString(name, age) {
+   return(`${this.name}, ${this.age}`)
+ }
+}
   
   /*
     TASK 2
