667e367b7bb0e6f4a78151f6907d473684cce9fc
unknown <twessar42@gmail.com>
2021-02-18 20:32:44 -0800

    all tests passing! woohoo

--- index.js
+++ index.js
@@ -187,7 +187,19 @@ class Student extends Lambdasian {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
-class ProjectManager {}
+class ProjectManager extends Instructor {
+	constructor(Object) {
+		super(Object);
+		this.gradClassName = Object.gradClassName;
+		this.favInstructor = Object.favInstructor;
+	}
+	standUp(channel) {
+		return `${this.name} announces to ${channel}, @channel standy times!`;
+	}
+	debugsCode(student, subject) {
+		return `${this.name} debugs ${student.name}'s code on ${subject}`;
+	}
+}
 /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
