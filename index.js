d5764af11d0fa23add4b4c79ee3e6a150f767a5c
Lara Hilborn <larahilborn@Laras-MacBook-Air.local>
2021-02-25 17:09:57 -0500

    completed first task

--- index.js
+++ index.js
@@ -42,7 +42,22 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = []
+    }
+    eat (food){
+      if(this.stomach.length < 10){
+        this.stomach.push(food);
+      }
+    }
+    poop (){
+      this.stomach = [];
+    }
+    toString (){
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
