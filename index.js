a3ebca54d6c90f5c4ed56738d19a08d93244707a
Lara Hilborn <larahilborn@Laras-MacBook-Air.local>
2021-02-25 18:32:26 -0500

    completed tasks two and three

--- index.js
+++ index.js
@@ -75,7 +75,30 @@ class Airplane {
   */
   
  class Car {
+    constructor (model, milesPerGallon){
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
     
+    fill(gallons){
+      this.tank = this.tank + gallons;
+    }
+
+    drive(distance){
+     const milesDriven = this.tank * this.milesPerGallon;
+     if(distance <= milesDriven){
+       this.odometer = this.odometer + distance;
+       this.tank = this.tank - (distance/this.milesPerGallon);
+     }
+     else{
+       this.odometer = this.odometer + milesDriven;
+       this.tank = 0;
+       return `I ran out of fuel at ${this.odometer} miles!`;
+     }
+    }
+
   }
   
   /*
@@ -91,7 +114,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attributes){
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+    }
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
   }
   
   /*
