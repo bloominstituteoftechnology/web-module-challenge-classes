a4ae1faa63847ee1045df4549e2904a4b72b1323
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-03-19 01:12:42 -0500

    project complete

--- index.js
+++ index.js
@@ -197,7 +197,18 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
+ class ProjectManager extends Instructor{
+   constructor(attributes){
+     super(attributes)
+     this.gradClassName = attributes.gradClassName;
+     this.favInstructor = attributes.favInstructor;
+   }
+   standUp(channel){
+     return `${this.name} announces to ${channel}, @channel standy times!`
+   }
+   debugsCode(student,subject){
+     return `${this.name} debugs ${student.name}'s code on ${subject}`
+   }
      
  }
   /*
