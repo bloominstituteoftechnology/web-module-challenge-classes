a7f463b73a7619f025b89f035f0be7698c635b2d
Debra <debrafon@gmail.com>
2021-03-18 22:02:30 -0400

    just 2 more fixes to go

--- index.js
+++ index.js
@@ -173,11 +173,11 @@ class Airplane {
     listSubjects () {
       return `${this.favSubjects}`
     }
-    PRAssignment (subject) {
-      `${student.name} has submitted a PR on ${subject}`
+    PRAssignment (student, subject) {
+      return `${student.name} has submitted a PR on ${subject}`
     }
-    sprintChallange (subject){
-      returns `${student.name} has begun sprint challenge on ${subject}`
+    sprintChallange (student, subject) {
+      return `${student.name} has begun sprint challenge on ${subject}`
     }
   }
   
@@ -194,9 +194,19 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
- }
+ class ProjectManager extends Instructor {
+   constructor (attributes) {
+     super (attributes);
+     this.gradClassName = attributes.gradClassName;
+     this.favInstructor = attributes.favInstructor;
+    }
+    standUp (channel) {
+      return `${this.name} announces to ${channel}, @channel standby times!`
+    }
+    debugsCode (student, subject) {
+      return `${this.name} debugs ${student.name}'s code on ${subject}`
+    }
+  }
   /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
