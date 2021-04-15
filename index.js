ef6f8ff6a7493f5841ded09dd494ae7d6650b93d
Amlak befekadu <amlakbarkon@gmail.com>
2021-04-15 13:21:06 -0400

    Task 4 done

--- index.js
+++ index.js
@@ -131,9 +131,20 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
- }
+          class Instructor extends Lambdasian {
+            constructor(Object) {
+              super(Object);
+              this.specialty = Object.specialty;
+              this.favLanguage = Object.favLanguage;
+              this.catchPhrase = Object.catchPhrase;
+            }
+            demo(subject) {
+              return `Today we are learning about ${subject}`;
+            }
+            grade(student, subject) {
+              return `${student.name} receives a perfect score on ${subject}`;
+            }
+          }
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
