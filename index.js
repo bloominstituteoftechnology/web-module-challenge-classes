dc93166fd7d0d86723e8b88971370619b9e6ccb7
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-05-13 20:11:04 -0700

    Finished up task 6, posting in help why my Task 2 Car Class Constructor has 3 fails

--- index.js
+++ index.js
@@ -210,8 +210,19 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(attr) {
+       super(attr);
+       this.gradClassName = attr.gradClassName;
+       this.favInstructor = attr.favInstructor;
+     }
+     standUp(channel){
+       return `${this.name} announces to ${channel}, @channel standby times!`
+       // why not this.channel??
+     }
+     debugsCode(student, subject){
+       return `${this.name} debugs ${student.name}'s code on ${subject}`
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
