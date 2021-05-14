a8c0a1b9756d716488423232b7f3fcb69d986422
Daniel <dek.kneale@gmail.com>
2021-05-13 18:10:49 -0700

    created a working  class for Person / task 1. created a working class for Car with model, milesPerGallon, tank, odometer, a function to fill tank and a function for driving distance(still incomplete)

--- index.js
+++ index.js
@@ -41,9 +41,25 @@ class Airplane {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- class Person {
-    
+class Person {
+  constructor(name, age) {
+      this.name = name,
+      this.age = age,
+      this.stomach = []
   }
+  eat(item){
+      if (this.stomach.length < 10){
+        return this.stomach.push(item)
+    }
+  }
+  poop(){
+    return this.stomach = [];
+  }
+  toString(name, age){
+    return `${this.name}, ${this.age}`
+  }
+}
+
   
   /*
     TASK 2
@@ -60,7 +76,22 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon){
+      this.model = model,
+      this.milesPerGallon = milesPerGallon,
+      this.tank = 0
+      this.odometer = 0
+    }
+    fill(gallons){
+      return this.tank = this.tank + gallons
+    }
+    drive(distance){
+     if (this.tank > distance / this.milesPerGallon){
+      return this.odometer = this.odometer + distance, this.tank = distance / this.milesPerGallon
+      }else if () {
+      return `I ran out of fuel at ${this.odometer} miles!`
+      }
+    }
   }
   
   /*
