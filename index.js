206e027b38d6b49d6956768a60ff99712d8ba76c
bwcummings1 <81990728+bwcummings1@users.noreply.github.com>
2021-06-18 01:37:05 -0500

    new changes

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ class Airplane {
      this.age = attributes.age;
      this.location = attributes.location;
    }
-   Lambdasian.prototype.speak = function (){
+   speak() {
      return `Hello my name is ${this.name}, I am from ${location}.`;
    }
   }
@@ -135,8 +135,16 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+   constructor (instructorAttributes){
+     super(instructorAttributes);
+     this.name = instructorAttributes.name;
+     this.age = instructorAttributes.age;
+     this.location = instructorAttributes.location;
+     this.specialty = instructorAttributes.specialty;
+     this.favLanguage = instructorAttributes.favLanguage;
+     this.catchPhrase = instructorAttributes.catchPhrase;
+   }
  }
   /*
     TASK 5
@@ -153,7 +161,7 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian {
      
  }
   
@@ -170,7 +178,7 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
+ class ProjectManager extends Instructor {
      
  }
   /*
